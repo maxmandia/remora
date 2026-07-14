0f8e980c5de07050301a053f90316e827afeedd8
Max Mandia <89795178+maxmandia@users.noreply.github.com>
2026-07-13 20:19:29 -0400

    feat: enable mixpanel session replays

--- apps/desktop/src/renderer/src/mixpanel-browser.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "mixpanel-browser/src/loaders/loader-module-core" {
-  import type { OverridedMixpanel } from "mixpanel-browser";
-
-  const mixpanel: OverridedMixpanel;
-  export default mixpanel;
-}
